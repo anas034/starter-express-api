89db36251d546e36919d724c6e4d24549411391b
Anas Shah <anasshahskype@gmail.com>
2023-02-02 12:53:29 +0500

    --

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
+require("dotenv").config()
+const http = require('http')
+const connectToMongo = require('./config/database')
 const express = require('express')
+const cors = require('cors')
+const all_routes = require('./routes/index')
+const redirect_route = require("./controllers/redirect_route")
+
+
+connectToMongo()
+
 const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
+const port = process.env.PORT || 5000
+
+app.use(cors())
+app.use(express.json())
+
+
+
+app.use(all_routes)
+app.get('*', redirect_route)
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
 })
-app.listen(process.env.PORT || 3000)
+
+
+module.exports = app
